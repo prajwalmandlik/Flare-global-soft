c19c6aa59cca36960a4da30512271688d4d24cdd
Prajwal Anil Mandlik <prajwalmandlik7@gmail.com>
2023-02-08 13:18:24 +0530

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-// import Footer from './components/footer/Footer';
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 
@@ -9,7 +9,7 @@ function App() {
     <>
       <Header />
       <Home />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,120 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
+import Logo from "../../assest/logo.png";
 
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
-  )
-}
+    <section className="container">
+      <div className="footer" >
+      <div className="footer-info">
+        <div className="footer-logo">
+          <div>
+            <img src={Logo} alt="" />
+          </div>
+          <div className="footer-logo-text">
+            <span>Flare</span>
+            <span>Global</span>
+            <span>Soft</span>
+          </div>
+        </div>
+        <div className="footer-description">
+        We are dedicated to delivering cutting-edge solutions that help businesses achieve their goals and reach new heights.
+        </div>
+        <div className="footer-social-links">
+          <ui className="footer-social-link">
+            <li>
+              <a href="#!">
+                <i class="bx bxl-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#!">
+              <i class='bx bxl-twitter' ></i>
+              </a>
+            </li>
+            <li>
+              <a href="#!">
+                <i class="bx bxl-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#!">
+                <i class="bx bxl-linkedin"></i>
+              </a>
+            </li>
+          </ui>
+        </div>
+      </div>
 
-export default Footer
+      <div className="footer-quick-link">
+        <h3>Quick Links</h3>
+        <ul>
+          <li>
+            <a href="#!" className="quick-link">
+              <i class="bx bx-chevron-right bx-flip-vertical"></i>About us
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="quick-link">
+              <i class="bx bx-chevron-right bx-flip-vertical"></i>Services
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="quick-link">
+              <i class="bx bx-chevron-right bx-flip-vertical"></i>Portfolio
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="quick-link">
+              <i class="bx bx-chevron-right bx-flip-vertical"></i>Our team
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="quick-link">
+              <i class="bx bx-chevron-right bx-flip-vertical"></i>Contact us
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="quick-link">
+              <i class="bx bx-chevron-right bx-flip-vertical"></i>Terms and
+              Condition
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="footer-contact">
+        <h3>Get in Touch</h3>
+        <ul>
+          <li>
+            <a href="#!">
+              <i class="bx bxs-map bx-flip-horizontal"></i>Nashik, Maharashtra
+            </a>
+          </li>
+          <li>
+            <a href="#!">
+              <i class="bx bxs-phone-call"></i>(+91) 9209237239
+            </a>
+          </li>
+          <li>
+            <a href="#!">
+              <i class="bx bxs-envelope"></i>flareglobalsoft@gmail.com
+            </a>
+          </li>
+          <li>
+            <a href="#!">
+              <i class="bx bx-globe"></i>www.flareglobalsoft.tech
+            </a>
+          </li>
+        </ul>
+      </div>
+      </div>
+
+      <div className="footer-copyrigth">
+        <p>©Copyright Flare Global Soft 2023 All Rights Reserved.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
