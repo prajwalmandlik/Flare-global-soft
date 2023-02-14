cbec348827340c191410f031f7069c5fb4a6389d
Prajwal Anil Mandlik <prajwalmandlik7@gmail.com>
2023-02-14 13:08:12 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
 import About from "./components/about/About";
-// import Contact from "./components/contact/Contact";
+import Contact from "./components/contact/Contact";
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
@@ -17,8 +17,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Services" element={<Services />} />
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/Contact" element={<Contact />} /> */}
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/Contact" element={<Contact />} />
         </Routes>
 
         <Footer />
