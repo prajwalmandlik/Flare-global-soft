43486bc1becc7ce88ceecc6dc4a3a0ed2fc8c84a
Prajwal Anil Mandlik <prajwalmandlik7@gmail.com>
2023-02-18 02:24:38 +0530

    app portfolio added

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -4,6 +4,8 @@ import Crypto from "../../assest/portfolio/crypto.jpg";
 import infinity from "../../assest/portfolio/infinity.jpg";
 import Shrimant from "../../assest/portfolio/shrimant.png";
 import Uniconrn from "../../assest/portfolio/unicorn.jpg";
+import Collage from "../../assest/portfolio/collage-app.jpg";
+import TicTacToe from "../../assest/portfolio/tic-tac-toe.jpg";
 
 
 export const projectsData = [
@@ -29,25 +31,39 @@ export const projectsData = [
         link: "https://crypto-scout.vercel.app/",
     },
     {
+        id: 4 ,
+        image : Collage,
+        title: 'Collage App',
+        category : 'app',
+        link: "https://flare-global-soft.vercel.app/",
+    },
+    {
+        id: 5 ,
+        image : TicTacToe,
+        title: 'Tic Tac Toe game',
+        category : 'app',
+        link: "https://flare-global-soft.vercel.app/",
+    },
+    {
         id: 6 ,
         image : infinity,
         title: 'Nashik based salon',
         category : 'graphic',
-        link: "https://crypto-scout.vercel.app/",
+        link: "https://flare-global-soft.vercel.app/",
     },
     {
         id: 7 ,
         image : Shrimant,
         title: 'photo studio',
         category : 'graphic',
-        link: "https://crypto-scout.vercel.app/",
+        link: "https://flare-global-soft.vercel.app/",
     },
     {
         id: 8 ,
         image : Uniconrn,
         title: 'Uniconrn',
         category : 'graphic',
-        link: "https://crypto-scout.vercel.app/",
+        link: "https://flare-global-soft.vercel.app/",
     },
 ];
 
